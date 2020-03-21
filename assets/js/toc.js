1fba10999e21d686070a8331683c19d31ba08c98
Allaudin <mallaudinqazi@gmail.com>
2020-03-21 14:48:52 +0100

    add initial toc

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,21 +1,75 @@
+const headingTags = 'h2[id], h3[id], h4[id]';
+
 window.addEventListener('DOMContentLoaded', () => {
+
+    generateToc();
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             const id = entry.target.getAttribute('id');
-            console.log("observer called");
-            if (entry.intersectionRatio > 0) {
-                document.querySelector(`div.toc a[href="#${id}"]`).classList.add('active');
-            } else {
-                document.querySelector(`div.toc  a[href="#${id}"]`).classList.remove('active');
+            const tocEntry = document.querySelector(`div#toc a[href="#${id}"]`);
+
+            if (tocEntry !== null) {
+                if (entry.intersectionRatio > 0) {
+                    tocEntry.classList.add('active');
+                } else {
+                    tocEntry.classList.remove('active');
+                }
             }
         });
     });
 
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('h2[id]').forEach((section) => {
-        console.log(section)
-        observer.observe(section);
-    });
+    document.querySelectorAll(headingTags)
+        .forEach((heading) => {
+            observer.observe(heading);
+        });
 
 });
+
+function generateToc() {
+    generateTocInId("toc");
+    generateTocInId("small-toc");
+}
+
+
+function generateTocInId(id) {
+    const firstLevelCounter = {count: 1};
+    const toc = document.getElementById(id);
+    const headings = document.querySelectorAll(headingTags);
+    toc.appendChild(createTocTitle());
+
+    for (let heading of headings) {
+        toc.appendChild(createTocEntryFromHeading(heading, firstLevelCounter));
+    }
+}
+
+function createTocTitle() {
+    const title = document.createElement("div");
+    title.innerHTML = "Table of content";
+    title.classList.add("title");
+    return title;
+}
+
+function createTocEntryFromHeading(heading, counter) {
+
+    const title = document.createElement("a");
+    let depth = heading.tagName.toLocaleLowerCase();
+    let prepend = "&bull;"; /* for h3 */
+
+    if (depth === "h2") {
+        const count = counter.count;
+        prepend = count + ". ";
+        counter.count = count + 1
+    }
+
+    if (depth === "h4") {
+        prepend = "&#8227;";
+    }
+
+    title.innerHTML = prepend + " " + heading.innerHTML;
+    title.href = '#' + heading.id;
+    title.classList.add(depth);
+
+    return title
+}
+
